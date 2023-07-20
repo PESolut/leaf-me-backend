1c1ec9c0b6f139cb4bf63e744eb5e40e2a2ddb6c
PESolut <jahaadpetty@pursuit.org>
2023-07-20 15:35:24 -0400

    page pagnitation added to allstoreitems route ("/allstoreitems/?page={page number})

--- controllers/allStoreItemsController.js
+++ controllers/allStoreItemsController.js
@@ -1,16 +1,64 @@
 const express = require('express')
 const storeItems = express.Router()
-
 const {getTrueAllStoreItems} = require('../queries/storeitems.js')
 
-storeItems.get('/', async (req, res) => {
-    const allStoreItems = await getTrueAllStoreItems()
+const isPageQueryADigit = (pageQuery) => {
+    const pageAsInteger = parseInt(pageQuery)
+    const digitRegex = /^\d$/;
+    return digitRegex.test(pageAsInteger);
+  };
 
-    if(allStoreItems.length){
-        res.status(200).json(allStoreItems)
-    } else {
-        res.status(500).json({Error: 'server error'})
+  storeItems.get('/', async (req, res) => {
+    try {
+        const allStoreItems = await getTrueAllStoreItems();
+
+        // check if the page parameter exists
+        if (req.query.page) {
+            // check that the page parameter is a valid digit
+            if (!isPageQueryADigit(req.query.page)) {
+                throw { message: 'page must be a valid digit!', status: 'error' };
+            }
+
+            // calculate the start and end index of the items for the requested page
+            const itemsPerPage = 12;
+            const page = parseInt(req.query.page);
+            const startIndex = (page - 1) * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+
+            // slice the allStoreItems array to get the items for the requested page
+            const storeItemsRange = allStoreItems.slice(startIndex, endIndex);
+
+            res.status(200).json(storeItemsRange);
+        } else {
+            res.status(200).json(allStoreItems);
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: error });
     }
-})
+});
+
+
+
+    
+
+    // // check to see if we have a response of storeItems
+    // if(allStoreItems.length){
+
+    //     console.log('page',req.query.page)
+        
+    //     // check if the query page parameters exist
+    //     if(req.query.page){
+    //         // Check if page parameter is a digit
+    //         if (!isDigit(parseInt(req.query.page))) {
+    //         throw { message: 'Start and end must be valid numbers!', status: 'error' };
+    //       }
+    //         console.log(req.query.page)          
+    //     }
+    //     res.status(200).json(allStoreItems)
+    // } else {
+    //     res.status(500).json({Error: 'server error'})
+    // }
+
 
 module.exports = storeItems
---
+++ db/moreItems.sql
@@ -0,0 +1,19 @@
+\c leafme_db
+
+INSERT INTO store_item (name, type, image, description, price, dispensary_ID)
+VALUES
+  ('Item 4', 'edible', 'item4.jpg', 'Description for Item 4', 8.49, 2),
+  ('Item 5', 'flower', 'item5.jpg', 'Description for Item 5', 11.99, 3),
+  ('Item 6', 'edible', 'item6.jpg', 'Description for Item 6', 7.99, 3),
+  ('Item 7', 'flower', 'item7.jpg', 'Description for Item 7', 10.49, 4),
+  ('Item 8', 'edible', 'item8.jpg', 'Description for Item 8', 9.99, 4),
+  ('Item 9', 'flower', 'item9.jpg', 'Description for Item 9', 12.99, 5),
+  ('Item 10', 'edible', 'item10.jpg', 'Description for Item 10', 6.49, 5),
+  ('Item 11', 'flower', 'item11.jpg', 'Description for Item 11', 8.99, 6),
+  ('Item 12', 'edible', 'item12.jpg', 'Description for Item 12', 7.49, 6),
+  ('Item 13', 'flower', 'item13.jpg', 'Description for Item 13', 11.99, 7),
+  ('Item 14', 'edible', 'item14.jpg', 'Description for Item 14', 10.99, 7),
+  ('Item 15', 'flower', 'item15.jpg', 'Description for Item 15', 9.49, 8),
+  ('Item 16', 'edible', 'item16.jpg', 'Description for Item 16', 8.49, 8),
+  ('Item 17', 'flower', 'item17.jpg', 'Description for Item 17', 10.99, 9),
+  ('Item 18', 'edible', 'item18.jpg', 'Description for Item 18', 12.99, 9);
