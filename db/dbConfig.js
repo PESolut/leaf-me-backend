2f0abfe07f430ddfd8ebc0951bc527dec8555a6b
PESolut <jahaadpetty@pursuit.org>
2024-04-21 16:31:11 -0400

    edwin saves the day with dbCONFIG

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,24 +1,23 @@
 const pgp = require("pg-promise")();
-require("dotenv").config();
-
-const cn = {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    ssl: {
-        rejectUnauthorized: false // For Heroku PostgreSQL databases, you may need to set this option
-    }
-};
-
-let db;
-
-try {
-    db = pgp(cn);
-    console.log("Connected to the database");
-} catch (error) {
-    console.error("Error connecting to the database:", error);
-}
-
-module.exports = db;
+import dotenv from 'dotenv';
+dotenv.config();
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
+const cn = DATABASE_URL
+	? {
+			connectionString: DATABASE_URL,
+			max: 30,
+			ssl: {
+				rejectUnauthorized: false,
+			},
+	  }
+	: {
+			host: PG_HOST,
+			port: PG_PORT,
+			database: PG_DATABASE,
+			user: PG_USER,
+	  };
+// alt from express docs
+// var db = pgp('postgres://username:password@host:port/database')
+const db = pgp(cn);
+export default db;
