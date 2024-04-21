e5665e8ad8a4fd9cf1bc49428fd9d20446f73b85
PESolut <jahaadpetty@pursuit.org>
2024-04-21 15:55:43 -0400

    db update

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,9 +6,19 @@ const cn = {
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD
+    password: process.env.PG_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false // For Heroku PostgreSQL databases, you may need to set this option
+    }
+};
+
+let db;
+
+try {
+    db = pgp(cn);
+    console.log("Connected to the database");
+} catch (error) {
+    console.error("Error connecting to the database:", error);
 }
 
-const db = pgp(cn)
-
-module.exports = db
+module.exports = db;
